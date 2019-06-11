3e6addab4264ec6ab491c43c410d6d4c7afb940f
shen wang <swan2892@uni.sydney.edu.au>
2019-06-11 15:50:33 +1000

    update immutable js

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,19 +1,15 @@
 import * as constants from './constants';
-
-const defeultState = {
+import { fromJS } from 'immutable';
+const defeultState = fromJS({
     focused: false
-};
+});
 
 export default (state = defeultState, action ) => {
     if (action.type === constants.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        return state.set('focused', true);
     }
     if (action.type === constants.SEARCH_BLUR) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false);
     }
     return state;
 }
